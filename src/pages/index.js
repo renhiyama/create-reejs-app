fca75b1c39114c819cfae3aa95b23f9388ae8ae1
Ren Hiyama <renhiyama@rovelstars.com>
2022-08-05 11:26:10 +0530

    dont ask me whats going on thanks

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,13 @@ let htm = await Import("htm");
 
 let html = htm.bind(h);
 export default function () {
-  return html`<h1>Hello from Reejs!</h1>`;
+  return html`<main className="h-screen bg-indigo-600 flex items-center justify-center">
+  <h1
+    className="font-bold /* you can even use inline comments */ text-white text-7xl"
+  >
+    Welcome To Ree.js!
+  </h1>
+</main>`;
 }
 
 /* REEJS_SERVER_SIDE_START */
@@ -12,7 +18,21 @@ export function head() {
 }
 
 export let config = {
-  hydrate: true,
+  hydrate: false,
+  runAfterInit: async()=>{
+    let c = await Import("https://esm.sh/canvas-confetti@1.5.1");
+    let main = $("main");
+    let previousC = "indigo";
+    let list=["indigo","red","purple","green","gray","blue"];
+    setInterval(()=>{
+      main.classList.remove(`bg-${previousC}-600`);
+      let newC = list[Math.floor(Math.random()*list.length)];
+      main.classList.add(`bg-${newC}-600`);
+      previousC = newC;
+    }, 1000);
+    c();
+  },
   cache: true,
+  twind: true
 }
 /* REEJS_SERVER_SIDE_END */
