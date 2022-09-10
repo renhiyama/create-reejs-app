40ccbaedaab7099899d5025988c4da56bf9c6c48
renhiyama <renhiyama@rovelstars.com>
2022-09-10 14:28:00 +0530

    updated to v0.4.9

---
+++ src/pages/**.jsx
@@ -0,0 +1,9 @@
+export default function () {
+    return <main className="min-h-screen bg-red-500">
+    <div className="flex items-center justify-center">
+      <h1 className="font-bold /* you can even use inline comments */ text-white text-7xl">
+        404
+      </h1>
+    </div>
+  </main>;
+  }
